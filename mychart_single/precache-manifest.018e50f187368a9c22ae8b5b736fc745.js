1c795524bbcacd40c17bdc6b075ebafca60c9e3b
CTL098775 <j747300k@gmail.com>
2021-05-02 20:40:46 +0800

    Elementary School v1

---
+++ mychart_single/precache-manifest.018e50f187368a9c22ae8b5b736fc745.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5489bb07ef8e844c911e77987d99adca",
+    "url": "/mychart_single/big-data.svg"
+  },
+  {
+    "revision": "cb44e3742d0d5283f18c",
+    "url": "/mychart_single/css/app.b09c5dc2.css"
+  },
+  {
+    "revision": "12fc2c8baeeb8a9e3f65",
+    "url": "/mychart_single/css/chunk-vendors.cbc2a074.css"
+  },
+  {
+    "revision": "6ec79e6580ecc8355084cb79cdf4c876",
+    "url": "/mychart_single/index.html"
+  },
+  {
+    "revision": "cb44e3742d0d5283f18c",
+    "url": "/mychart_single/js/app.60b989ed.js"
+  },
+  {
+    "revision": "12fc2c8baeeb8a9e3f65",
+    "url": "/mychart_single/js/chunk-vendors.33873b8b.js"
+  },
+  {
+    "revision": "65fe7b214cf992ec3e373c3bdf5672a7",
+    "url": "/mychart_single/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/mychart_single/robots.txt"
+  }
+]);
